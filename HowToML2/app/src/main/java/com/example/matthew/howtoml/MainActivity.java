acfc62761bf06fd976a2fd29a9af1ba6582fec00
Matthew Whalley <mymatt11@gmail.com>
2016-02-29 11:13:09 +0000

    updated the main header to have a logo, and polished the template for the lessons. Also replacing the navigation buttons at the bottom of that page

--- HowToML2/app/src/main/java/com/example/matthew/howtoml/MainActivity.java
+++ HowToML2/app/src/main/java/com/example/matthew/howtoml/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.matthew.howtoml;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,7 +22,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.drawable.logo);
+        toolbar.setLogoDescription("HTML Logo");
         setSupportActionBar(toolbar);
+        toolbar.setBackgroundColor(Color.parseColor("#ff6600"));
 
       /*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
